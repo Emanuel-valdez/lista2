6c96eedff94e8cd58ebc3d8b25c54e59daa87f59
emanuel valdez <ing.valdezemnuel@outlook.com>
2026-03-21 20:40:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lista2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lista de numeros enteros");
+
+            List<int> numeros = new List<int>();
+
+            numeros.Add(23);
+            numeros.Add(24);
+            numeros.Add(76);
+            numeros.Add(4);
+            numeros.Add(5);
+
+            foreach (var n in numeros)
+            {
+                
+            }
+
+
+
+        }
+    }
+}
